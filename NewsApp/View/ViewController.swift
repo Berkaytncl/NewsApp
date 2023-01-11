f5b7f9c0f37f59d11a403b49db78aea495d65cd6
Berkay Tuncel <berkaytncl98@gmail.com>
2023-01-11 17:10:41 +0300

    Changed file locations

--- NewsApp/View/ViewController.swift
+++ NewsApp/View/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    private var newsTableViewModel: NewsTableViewModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -30,18 +32,34 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.compactAppearance = appearance
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
         
+        takeData()
+    }
+    
+    func takeData() {
+        let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/BTK-iOSDataSet/master/dataset.json")
+        
+        WebService().downloadNews(url: url!) { news in
+            if let news = news {
+                self.newsTableViewModel = NewsTableViewModel(newsList: news)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return newsTableViewModel == nil ? 0 : self.newsTableViewModel!.numberOfRowsInSection()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsCell
-        cell.titleLabel.text = "1"
-        cell.storyLabel.text = "a"
+        let newsViewModel = self.newsTableViewModel?.newsAtIndexPath(indexPath.row)
+        cell.titleLabel.text = newsViewModel?.title
+        cell.storyLabel.text = newsViewModel?.story
         return cell
     }
     
