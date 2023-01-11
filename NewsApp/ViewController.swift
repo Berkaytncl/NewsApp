4b1ab6f668836e98728dff9fd16355c45285f6a5
Berkay Tuncel <berkaytncl98@gmail.com>
2023-01-11 13:12:59 +0300

    Updated UI

--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        self.navigationItem.title = "News"
+        
+        let appearance = UINavigationBarAppearance()
+        
+        appearance.backgroundColor = UIColor(named: "mainColor")
+        appearance.titleTextAttributes = [.foregroundColor:UIColor(named: "foregroundColor1")!, .font:UIFont(name: "Newsreader-Bold", size: 24)!]
+        
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsCell
+        cell.titleLabel.text = "1"
+        cell.storyLabel.text = "a"
+        return cell
+    }
+    
+}
